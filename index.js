bcce612c4dfe233762b1588347b23a7d6e94fb25
Lee Brooks <leebrooks93@gmail.com>
2017-09-12 15:10:28 +0100

    Updated JSDoc rules
    * Disabled required param & return descriptions

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "extends": "airbnb-base",
-    "env":{
-        "browser": true,
-        "node": true,
-        "jest": true
+    'parser': 'babel-eslint',
+    'extends': 'airbnb-base',
+    'env':{
+        'browser': true,
+        'node': true,
+        'jest': true
     },
-    "rules": {
-        "max-len": [2, 120],
-        "indent": [2, 4, { "SwitchCase": 1, "VariableDeclarator": 1, "outerIIFEBody": 1 }],
-        "arrow-parens": [2, "as-needed"],
-        "valid-jsdoc": 1
+    'rules': {
+        'max-len': [2, 120],
+        'indent': [2, 4, {
+            'SwitchCase': 1,
+            'VariableDeclarator': 1,
+            'outerIIFEBody': 1
+        }],
+        'arrow-parens': [2, 'as-needed'],
+        'valid-jsdoc': [1, {
+            'requireParamDescription': false,
+            'requireReturnDescription': false
+        }]
     },
-    "plugins": [
-        "import"
+    'plugins': [
+        'import'
     ]
 };
