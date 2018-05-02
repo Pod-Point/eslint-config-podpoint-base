8fc3f6df926e922c24d163b9d58a9b03a69ff855
threesquared <ben@3sq.re>
2018-05-02 13:49:46 +0100

    Update comma-dangle rules

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ module.exports = {
     "rules": {
         "max-len": [2, 120],
         "indent": [2, 4, { "SwitchCase": 1, "VariableDeclarator": 1, "outerIIFEBody": 1 }],
-        "arrow-parens": [2, "as-needed"]
+        "arrow-parens": [2, "as-needed"],
+        "comma-dangle": ["error", {
+            "arrays": "always-multiline",
+            "objects": "always-multiline",
+            "imports": "always-multiline",
+            "exports": "always-multiline",
+            "functions": "ignore"
+        }]
     },
     "plugins": [
         "import"
